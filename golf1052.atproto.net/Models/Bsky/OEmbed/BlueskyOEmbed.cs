94965ee19a0d93e63d6b0db8c424671aa4e14bea
Sanders Lauture <golf1052@msn.com>
2025-07-22 01:01:00 -0700

    Add Bluesky oEmbed class

---
+++ golf1052.atproto.net/Models/Bsky/OEmbed/BlueskyOEmbed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace golf1052.atproto.net.Models.Bsky.OEmbed
+{
+    public class BlueskyOEmbed
+    {
+        public string Type { get; set; } = string.Empty;
+        public string Version { get; set; } = string.Empty;
+        public string? Title { get; set; }
+        public string AuthorName { get; set; } = string.Empty;
+        public string AuthorUrl { get; set; } = string.Empty;
+        public string ProviderName { get; set; } = string.Empty;
+        public string ProviderUrl { get; set; } = string.Empty;
+        public int CacheAge { get; set; }
+        public string Html { get; set; } = string.Empty;
+        public int Width { get; set; }
+        public int? Height { get; set; }
+    }
+}
